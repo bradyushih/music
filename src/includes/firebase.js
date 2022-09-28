710de4c27a0d99e3d308a45ed668522eba3435b9
iambrad <brad771012@gmail.com>
2022-09-29 00:07:05 +0800

    upload file

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app"; // new version 'firebase/compact/app'
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpVyfFkB1EzvL5E1Mv-4Tiw4C77neiXSo", // connect to firebase
@@ -16,7 +17,9 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
+const songsCollection = db.collection("songs");
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection, storage, songsCollection };
